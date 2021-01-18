4944c461a55725524530523a2121943cd6f00412
Jacob Trock <jtbosshku@gmail.com>
2021-01-18 09:24:20 -0500

    fix optimizations

--- cmp.sh
+++ cmp.sh
@@ -1,4 +1,4 @@
 cd readability/ && git submodule update && cd ..
-npx google-closure-compiler --js ./readability/Readability.js --js_output_file ./unl.js --compilation_level ADVANCED_OPTIMIZATIONS --rewrite_polyfills --jscomp_off undefinedVars
+npx google-closure-compiler --js ./readability/Readability.js --js_output_file ./unl.js --compilation_level SIMPLE_OPTIMIZATIONS --rewrite_polyfills
 cat prepend.txt unl.js > Readable_closure.js
 rm unl.js
